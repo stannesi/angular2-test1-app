ee8ff6d34cdc22f0241b79647eb128548c3f5da7
stannesi <stannesi@gmail.com>
2017-01-04 08:30:51 -0600

    .: bracket commit test :.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'HELLO Angular 2! It app works! well';
+  title = 'HELLO Angular 2! It app works! wellsss';
 }
